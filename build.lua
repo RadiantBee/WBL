55147f426f9c28afe74db889f88002941fa62571
MaxPan <inator@ukr.net>
2025-02-07 00:45:02 +0100

    intoducing ellraiser/love-build

---
+++ build.lua
@@ -0,0 +1,11 @@
+return {
+	name = "WBL",
+	developer = "MaxPan",
+	output = "build",
+	version = "0.2tb", -- tb -> test build
+	love = "11.5",
+	ignore = { "build" },
+	icon = "ico/webby.png",
+	identifier = "love.maxpan.wbl",
+    platforms = {'windows','macos','linux'}
+}
